2ad782e07003396c3913e6eb4a5038043906b2ac
Philip Johnston <pjohn896@gmail.com>
2024-10-11 16:53:35 -0400

    Big theme push

---
+++ _posts/2024-10-11-first-post.md
@@ -0,0 +1,1 @@
+this is a post and it still works
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "First Post",
+        "excerpt":"this is a post and it still works  ","categories": [],
+        "tags": [],
+        "url": "/first-post/",
+        "teaser": null
+      }]
